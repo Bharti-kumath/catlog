3b47ab5b0415cc3ef8c01f87f763374cec26f616
Bharti Kumath <77137277+Bharti-kumath@users.noreply.github.com>
2021-05-29 18:30:25 +0530

    Add files via upload

---
+++ menu/app.js
@@ -0,0 +1,161 @@
+const menu = [{
+
+    id: 1,
+    title: "kumkum",
+    category: "kumkum",
+    price: "Rs.550",
+    img: "kohi.png",
+    desc: "this is a semi weighless saree with 6 meter long.it has maharani lace with blouse"
+
+
+},
+{
+    id: 2,
+    title: "pari",
+    category: "pari",
+    price: "Rs.600",
+    img: "https://scontent.fstv2-1.fna.fbcdn.net/v/t1.6435-9/104290421_103021854795914_9191170471861443808_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=3dFWwbBAon4AX8PFBty&tn=aDc6vfKK4ou5mxzL&_nc_ht=scontent.fstv2-1.fna&oh=f90e4d084db5f08b6b8740bdce11637a&oe=60D45B9A",
+    desc: "this is a semi weighless saree with 6 meter long.it has maharani lace with blouse"
+
+},
+{
+    id: 3,
+    title: "kumkum",
+    category: "kumkum",
+    price: "Rs.550",
+    img: "https://scontent.fstv2-1.fna.fbcdn.net/v/t1.6435-9/104274390_103033234794776_4499630999464482273_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=F_WBbOiKoicAX9dBMc2&_nc_oc=AQlE4IEYDwIHXUOar-GqlZOhM3JAmWsVETm9PoElOwUsAaTQguAAMalsFZn0UWI9Zpg&_nc_ht=scontent.fstv2-1.fna&oh=d892ebabbc691fcd8478733b7e9b30b2&oe=60D513DB",
+    desc: "this is a semi weighless saree with 6 meter long.it has maharani lace with blouse"
+
+},
+{
+    id: 4,
+    title: "pari",
+    category: "pari",
+    price: "Rs.600",
+    img: "https://scontent.fstv2-1.fna.fbcdn.net/v/t1.6435-0/s600x600/105561273_110680024030097_5828935155797168264_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=brRyGoMqkBQAX-3SZ7s&tn=aDc6vfKK4ou5mxzL&_nc_ht=scontent.fstv2-1.fna&tp=7&oh=7d2d80102e7a3ac8a87e8c5113ea7420&oe=60D50DA3",
+    desc: "this is a semi weighless saree with 6 meter long.it has maharani lace with blouse"
+
+},
+{
+    id: 5,
+    title: "kumkum",
+    category: "kumkum",
+    price: "Rs.650",
+    img: "https://scontent.fstv2-1.fna.fbcdn.net/v/t1.6435-9/106366399_110695384028561_6681488471499799311_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=H30QI1LeWm4AX9HCpbO&_nc_oc=AQkPsJ7M52pxX6AB4HaksI7VGbAEo37sa77fvRalyvZFZK_LYe3VLFKFK1Nt5559egQ&_nc_ht=scontent.fstv2-1.fna&oh=9cc8c0179c56e10d93beaf7b0a279fa6&oe=60D4C7A5",
+    desc: "this is a semi weighless saree with 6 meter long.it has maharani lace with blouse"
+},
+
+{
+    id: 6,
+    title: "parichay",
+    category: "parichay",
+    price: "Rs.950",
+    img: "https://scontent.fstv2-1.fna.fbcdn.net/v/t1.6435-9/104169053_103060311458735_9035356500114968952_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=MndCoVw5NUIAX_9G8h4&_nc_ht=scontent.fstv2-1.fna&oh=6cad5079cf3120ded9bfc4f00923c115&oe=60D573F9",
+    desc: "this is a semi weighless saree with 6 meter long.it has maharani lace with blouse"
+},
+{
+    id: 7,
+    title: "pari",
+    category: "pari",
+    price: "Rs.600",
+    img: "https://scontent.fstv2-1.fna.fbcdn.net/v/t1.6435-9/105574596_110675860697180_3054251574093683319_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=KNsGJra6atsAX81z2XA&_nc_ht=scontent.fstv2-1.fna&oh=a276bb7c48b7aa92557ed165c0348541&oe=60D72CAC",
+    desc: "this is a semi weighless saree with 6 meter long.it has maharani lace with blouse"
+},
+
+{
+    id: 8,
+    title: "kumkum",
+    category: "kumkum",
+    price: "Rs.650",
+    img: "https://scontent.fstv2-1.fna.fbcdn.net/v/t1.6435-9/104589840_103078944790205_32367579063310668_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=TXwpCE-2oeUAX9GbxnV&_nc_ht=scontent.fstv2-1.fna&oh=e032672adf1588083c26a66bab6ce363&oe=60D73D3F",
+    desc: "this is a semi weighless saree with 6 meter long.it has maharani lace with blouse"
+},
+{
+    id: 9,
+    title: "parichay",
+    category: "parichay",
+    price: "Rs.950",
+    img: "https://scontent.fstv2-1.fna.fbcdn.net/v/t1.6435-9/104475417_102796238151809_3789345207300192407_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=QzQLtJ8o_WEAX9ipZNf&_nc_ht=scontent.fstv2-1.fna&oh=cf935d9ed0a4e8aa2517ec4ec0410c76&oe=60D757B5",
+    desc: "this is a semi weighless saree with 6 meter long.it has maharani lace with blouse"
+
+}
+
+
+
+
+
+
+
+]
+
+// get parent element
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+// display all items when page loads
+window.addEventListener("DOMContentLoaded", function () {
+    diplayMenuItems(menu);
+    displayMenuButtons();
+});
+
+function diplayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function (item) {
+        // console.log(item);
+
+        return `<article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`;
+    });
+    displayMenu = displayMenu.join("");
+    // console.log(displayMenu);
+
+    sectionCenter.innerHTML = displayMenu;
+}
+function displayMenuButtons() {
+    const categories = menu.reduce(
+        function (values, item) {
+            if (!values.includes(item.category)) {
+                values.push(item.category);
+            }
+            return values;
+        },
+        ["all"]
+    );
+    const categoryBtns = categories
+        .map(function (category) {
+            return `<button type="button" class="filter-btn" data-id=${category}>
+          ${category}
+        </button>`;
+        })
+        .join("");
+
+    btnContainer.innerHTML = categoryBtns;
+    const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+    console.log(filterBtns);
+
+    filterBtns.forEach(function (btn) {
+        btn.addEventListener("click", function (e) {
+            // console.log(e.currentTarget.dataset);
+            const category = e.currentTarget.dataset.id;
+            const menuCategory = menu.filter(function (menuItem) {
+                // console.log(menuItem.category);
+                if (menuItem.category === category) {
+                    return menuItem;
+                }
+            });
+            if (category === "all") {
+                diplayMenuItems(menu);
+            } else {
+                diplayMenuItems(menuCategory);
+            }
+        });
+    });
+}
